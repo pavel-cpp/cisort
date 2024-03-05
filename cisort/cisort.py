05d76f0f0948c2653a11ca33adc5ced407b3d1dd
Pavel Remdenok <itztoru@vk.com>
2024-03-05 21:30:14 +0000

    load_json done

--- cisort/cisort.py
+++ cisort/cisort.py
@@ -2,70 +2,62 @@ import os
 import sys
 
 from .parse import get_includes
+from .config import configure_argument_parser, configure_logging
 
 
 def sort_all(includes):
     for include in includes:
-        coords = include.pop()
-        include.sort()
-        include.append(coords)
+            coords = include.pop()
+            include.sort()
+            include.append(coords)
     return includes
 
 
 def insert_includes(includes, path):
     with open(path) as file:
-        data = file.readlines()
-        for include in includes:
-            for i in range(include[-1][0], include[-1][1]):
-                data[i] = f'#include {include[i - include[-1][0]]}\n'
+            data = file.readlines()
+            for include in includes:
+                    for i in range(include[-1][0], include[-1][1]):
+                            data[i] = f'#include {include[i - include[-1][0]]}\n'
     with open(path, 'w') as file:
-        file.writelines(data)
+            file.writelines(data)
 
 
 def get_files(directory: str = '.',
               flags: "list | None" = None,
               files: "list | None" = None) -> list:
     if flags is None:
-        flags = []
+            flags = []
     if files is None:
-        files = []
+            files = []
 
     for file in os.listdir(directory):
-        path = f"{directory}/{file}"
-        if os.path.isdir(path) and '-r' in flags:
-            get_files(path, flags, files)
-        elif file.split('.')[-1] in ('c', 'cpp', 'h', 'hpp'):
-            if '-ls' in flags:
-                print(f'Add to cisorting list {path}')
-            files.append(path)
+            path = f"{directory}/{file}"
+            if os.path.isdir(path) and '-r' in flags:
+                    get_files(path, flags, files)
+            elif file.split('.')[-1] in ('c', 'cpp', 'h', 'hpp'):
+                    if '-ls' in flags:
+                            print(f'Add to cisorting list {path}')
+                    files.append(path)
     return files
 
 
 def is_correct_flags(flags: list) -> bool:
     available_flags = ('-r', '-ls', '-h', '--help')
     for flag in flags:
-        if flag not in available_flags:
-            print(
-                f'Flag "{flag}" is incorrect!\n'
-                f'Try:\n\tcisort --help'
-            )
-            return False
+            if flag not in available_flags:
+                    print(
+                            f'Flag "{flag}" is incorrect!\n'
+                            f'Try:\n\tcisort --help'
+                    )
+                    return False
     return True
 
 
 def cisort():
-    args = sys.argv[1:]
-
-    if '-h' in args or '--help' in args:
-        print(
-            'Using:\n\n'
-            '\tcisort [flags] [path]\n\n'
-            'Flags:\n'
-            '\t-r - recursive searching C/C++ files\n'
-            '\t-ls - show info about sorted files\n'
-            '\t-h --help - to get help'
-        )
-        return
+    configure_logging()
+    arg_parser = configure_argument_parser()
+    args = arg_parser.parse_args()
 
     if sys.argv[-1][0] == '-' or not args:
         print('Start cisearching...')
@@ -75,14 +67,13 @@ def cisort():
         files = get_files(directory=args[-1], flags=args[:-1])
     else:
         return
-
     print('Start cisorting...')
     for file in files:
-        if '-ls' in args:
-            print(f'Cisorting {file}')
-        insert_includes(sort_all(get_includes(file)), file)
+            if '-ls' in args:
+                    print(f'Cisorting {file}')
+            insert_includes(sort_all(get_includes(file)), file)
     print(f'{len(files)} files are cisorted!')
 
 
 if __name__ == '__main__':
-    cisort()
+        cisort()
--- cisort/config.py
+++ cisort/config.py
@@ -2,7 +2,6 @@ import argparse
 import logging
 from logging.handlers import RotatingFileHandler
 
-from constants import FILE_CASE, LOG_DIR, LOG_FILE, PRETTY_CASE
 
 LOG_FORMAT = '"[%(levelname)s] %(asctime)s - %(message)s"'
 DT_FORMAT = '%d.%m.%Y %H:%M:%S'
@@ -41,14 +40,10 @@ def configure_argument_parser():
         return parser
 
 def configure_logging():
-    LOG_DIR.mkdir(exist_ok=True)
-    
-    rotating_handler = RotatingFileHandler(
-        LOG_FILE, maxBytes=10 ** 6, backupCount=5
-    )
+    # Возможно stream=sys.stdout
     logging.basicConfig(
         datefmt=DT_FORMAT,
         format=LOG_FORMAT,
         level=logging.INFO,
-        handlers=(rotating_handler, logging.StreamHandler())
+        handlers=(logging.StreamHandler(),)
     )
